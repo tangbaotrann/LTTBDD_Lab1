0135b6142f044e6af85cb9090fa3a9828d4a17e9
Bao Tran <baotran2k1@outlook.com>
2022-02-16 21:47:29 +0700

    update_v2

---
+++ app/src/main/java/com/example/myapplication/MainActivityLab01_1e.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLab01_1e extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab011e);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/MainActivityLab01_Comments.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivityLab01_Comments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_lab01_comments);
+    }
+}
